93d8d5ab41e42859edb9adc59bdb1a0c1acf26c6
闫鹏磊 <75997533@qq.com>
2017-10-16 13:52:10 +0800

    spring boot redis

---
+++ ymq-redis-spring-boot/src/main/java/io/ymq/redis/run/Application.java
@@ -0,0 +1,18 @@
+package io.ymq.redis.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述:
+ *
+ * @author yanpenglei
+ * @create 2017-10-16 13:19
+ **/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
