6d01a0768eeffa7720e9ea1b97f1c1f045b65251
闫鹏磊 <75997533@qq.com>
2017-10-16 15:00:15 +0800

    spring boot redis

---
+++ ymq-redis-spring-boot/src/main/java/io/ymq/redis/config/RedisAutoConfiguration.java
@@ -0,0 +1,17 @@
+package io.ymq.redis.config;
+
+import io.ymq.redis.utils.CacheUtils;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * 描述:
+ *
+ * @author yanpenglei
+ * @create 2017-10-16 14:50
+ **/
+@Configuration
+@Import({RedisConfig.class, CacheUtils.class})
+public class RedisAutoConfiguration {
+
+}
