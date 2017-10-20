f8fb426ba293490edba005214936533b98a66794
闫鹏磊 <75997533@qq.com>
2017-10-20 17:47:33 +0800

    Spring Boot 中使用 RabbitMQ

--- ymq-rabbitmq-spring-boot/src/main/java/io/ymq/rabbitmq/topic/TopicReceiver2.java
+++ ymq-rabbitmq-spring-boot/src/main/java/io/ymq/rabbitmq/topic/TopicReceiver2.java
@@ -4,12 +4,6 @@ import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-/**
- * 描述: topic.mss 主题
- *
- * @author yanpenglei
- * @create 2017-10-20 15:02
- **/
 
 @Component
 @RabbitListener(queues = "topic.messages")
