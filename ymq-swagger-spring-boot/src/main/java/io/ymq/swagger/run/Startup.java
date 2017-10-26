c098d3ff20c16aaedfc86c59d4dd40f3b48e7d08
闫鹏磊 <75997533@qq.com>
2017-10-26 17:06:51 +0800

    项目重构

--- ymq-swagger-spring-boot/src/main/java/io/ymq/swagger/run/Startup.java
+++ ymq-swagger-spring-boot/src/main/java/io/ymq/swagger/run/Startup.java
@@ -1,17 +1,17 @@
-package io.ymq.rabbitmq.run;
+package io.ymq.swagger.run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
- * 描述: 启动服务
+ * 描述:启动服务
  *
- * @author: yanpenglei
- * @create: 2017/10/23 14:14
- */
+ * @author yanpenglei
+ * @create 2017-10-26 16:37
+ **/
 @SpringBootApplication
-@ComponentScan(value = {"io.ymq.rabbitmq"})
+@ComponentScan(value = {"io.ymq.swagger"})
 public class Startup {
 
     public static void main(String[] args) {
